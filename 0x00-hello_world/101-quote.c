65b8085b2b72c0f147baae7ce62676b740e1296a
Mahmoud EL-Sanour <mahmoudsanour@gmail.com>
2023-03-16 08:25:55 -0700

    Edit: 101-quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -10,6 +10,6 @@ int main(void)
 {
 	char buf[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
 
-	write(1, buf, strnlen(buf));
+	write(1, buf, strnlen(buf,sizeof(buf)));
 	return (1);
 }
