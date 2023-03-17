b1d2816bd0ebcbb85f4c21b70a2c6d7ba8e23df1
Mahmoud EL-Sanour <mahmoudsanour@gmail.com>
2023-03-17 06:30:19 -0700

    Edit: 3-print_alphabets.c

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -8,13 +8,13 @@
 int main(void)
 {
 	char *alphabet = "abcdefghijklmnopqrstuvwxyz";
-	char *ALPHABET = "ABCDEFGHIJKLMNOPQRSTUVWXYZ$";
+	char *ALPHABET = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
 	int i;
 
 	for (i = 0; i < 26; i++)
 		putchar(alphabet[i]);
-	for (i = 0; i < 27; i++)
-                putchar(ALPHABET[i]);
+	for (i = 0; i < 26; i++)
+		putchar(ALPHABET[i]);
 	putchar('\n');
 	return (0);
 }
