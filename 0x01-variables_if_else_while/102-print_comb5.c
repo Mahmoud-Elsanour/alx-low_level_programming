b380e96d867808db6717071ae597d99589bb31c3
Mahmoud EL-Sanour <mahmoudsanour@gmail.com>
2023-03-17 14:18:01 -0700

    Add: 102-print_comb5.c

---
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -0,0 +1,50 @@
+#include <stdio.h>
+
+/**
+ * main - the main function
+ *
+ * Return: 0
+ */
+int main(void)
+{
+	int i;
+	int j;
+	int k;
+	int l;
+	int start_i;
+	int start_j;
+	int start_k;
+	int start_l;
+
+	start_i = 1;
+	start_j = 0;
+	start_k = 0;
+	start_l = 0;
+	for (l = start_l; l < 10; l++)
+	{
+		for (k = start_k; k < 10; k++)
+		{
+			for (j = start_j; j < 10; j++)
+			{
+				for (i = start_i; i < 10; i++)
+				{
+					putchar(48 + l);
+					putchar(48 + k);
+					putchar(' ');
+					putchar(48 + j);
+					putchar(48 + i);
+					putchar(',');
+					putchar(' ');
+				}
+			}
+			start_i++;
+			if (start_i == 10)
+			{
+				start_i = 0;
+				start_j++;
+			}
+		}
+	}
+	putchar('\n');
+	return (0);
+}
