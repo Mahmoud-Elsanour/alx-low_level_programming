d7e31d718dcc9b99f302b43e9ac991a8bb9da825
Mahmoud EL-Sanour <mahmoudsanour@gmail.com>
2023-03-21 06:47:50 -0700

    Edit: 3-islower.c

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -3,11 +3,14 @@
 
 /**
  * _islower - prints alphabet
+ *
+ * @c: checks input of function
+ *
  * Return: 0
  */
 int _islower(int c)
 {
-	if (islower (c) == 0)
+	if (islower(c) == 0)
 		return (0);
 	else
 		return (1);
