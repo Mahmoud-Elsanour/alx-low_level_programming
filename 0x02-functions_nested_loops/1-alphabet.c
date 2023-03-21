3b95e3fb958420877fd0b2a6ae769444ae76a02e
Mahmoud EL-Sanour <mahmoudsanour@gmail.com>
2023-03-21 05:50:28 -0700

    Edit: 1-alphabet.c

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -9,11 +9,10 @@ void print_alphabet(void)
 	char *sentence = "abcdefghijklmnopqrstuvwxyz";
 	int i = 0;
 
-	while (i < 24)
+	while (i < 26)
 	{
 		_putchar(sentence[i]);
 		i++;
 	}
 	_putchar('\n');
-	return (0);
 }
