ab4a99303e902cc07a67dfbb4684a482409606f1
Mahmoud EL-Sanour <mahmoudsanour@gmail.com>
2023-03-21 05:36:39 -0700

    Add: 0-putchar.c

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,4 +1,4 @@
-#include<stdio.h>
+#include"main.h"
 
 /**
  * main - prints a line
@@ -6,6 +6,14 @@
  */
 int main(void)
 {
-	puts("_putchar\n");
+	char *sentence = "_putchar";
+	int i = 0;
+
+	while (i < 8)
+	{
+		_putchar(sentence[i]);
+		i++;
+	}
+	_putchar('\n');
 	return (0);
 }
