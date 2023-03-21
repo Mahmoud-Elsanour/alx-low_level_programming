f2624e0a51f32b9d4dde278bcd671fba3befc89b
Mahmoud EL-Sanour <mahmoudsanour@gmail.com>
2023-03-21 05:57:48 -0700

    Add: 2-print_alphabet_x10.c

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,24 @@
+#include"main.h"
+
+/**
+ * print_alphabet - prints alphabet
+ * Return: 0
+ */
+void print_alphabet_x10(void)
+{
+	char *sentence = "abcdefghijklmnopqrstuvwxyz";
+	int i;
+	int j = 0;
+
+	while (j < 10)
+	{
+		i = 0;
+		while (i < 26)
+		{
+			_putchar(sentence[i]);
+			i++;
+		}
+		_putchar('\n');
+	j++;
+	}
+}
