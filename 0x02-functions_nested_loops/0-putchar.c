5dbcca2ee9a26f076537458a288ae73173922688
Mahmoud EL-Sanour <mahmoudsanour@gmail.com>
2023-03-21 05:08:41 -0700

    Add: 0-putchar.c

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+
+/**
+ * main - prints a line
+ * Return: 0
+ */
+int main(void)
+{
+	puts("_putchar\n");
+	return (0);
+}
---
+++ 0x02-functions_nested_loops/README.md
@@ -0,0 +1,1 @@
+# A directory for my third project, "0x02-functions_nested_loops"
