c7b23d381e1c9712f33da7bb79d3fc66bf545c9a
Mahmoud EL-Sanour <mahmoudsanour@gmail.com>
2023-03-21 08:49:41 -0700

    Add: 9-times_table.c

---
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -0,0 +1,32 @@
+#include"main.h"
+
+/**
+ * times_table - print the 9 times table
+ *
+ * Return: void
+ */
+void times_table(void)
+{
+	int row = 0;
+	int column = 0;
+
+	while (row < 10)
+	{
+		while (column < 10)
+		{
+			if (column > 0)
+			{
+				elem = row * column;
+				_putchar(',');
+				_putchar(' ');
+				if (elem / 10 == 0)
+					_putchar(' ');
+				else
+					_putchar(elem / 10 + '0');
+			}
+			_putchar(elem % 10 + '0');
+			column++;
+		}
+		row++;
+	}
+}
