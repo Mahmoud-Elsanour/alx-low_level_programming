ade85f7e1e383503e122de1215e456f918b792d1
Mahmoud EL-Sanour <mahmoudsanour@gmail.com>
2023-03-27 10:50:59 -0700

    Add: 5-rev_string.c

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,30 @@
+#include"main.h"
+#include<unistd.h>
+
+/**
+ * rev_string - print a string reversed
+ *
+ * @s: the string
+ *
+ * Return: void
+ */
+void rev_string(char *s)
+{
+	int i = 0;
+	char c = s[i];
+	char *tmp = s;
+	int strln;
+
+	while (c != '\0')
+	{
+		i++;
+		c = s[i];
+	}
+	strln = i;
+	c = s[--i];
+	while (i != -1)
+	{
+		tmp[strln - 1 - i] = s[i];
+		i--;
+	}
+}
