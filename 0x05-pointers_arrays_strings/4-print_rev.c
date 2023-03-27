c86e2c947cb759c63d2986266b02aedfcfd78bb6
Mahmoud EL-Sanour <mahmoudsanour@gmail.com>
2023-03-27 10:11:23 -0700

    Edit: void print_rev(char *s);

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -10,14 +10,14 @@
  */
 void print_rev(char *s)
 {
-	int i = _strlen(*s);
-	char c = str[i];
+	int i = _strlen(s);
+	char c = s[i];
 
 	while (i != -1)
 	{
 		write(1, &c, 1);
 		i--;
-		c = str[i];
+		c = s[i];
 	}
 	c = '\n';
 	write(1, &c, 1);
