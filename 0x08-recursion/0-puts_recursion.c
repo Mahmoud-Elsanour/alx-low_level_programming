7a3431140fc4ca2aa9bb0fd4d144ac1bc302ac89
Mahmoud EL-Sanour <mahmoudsanour@gmail.com>
2023-04-05 17:11:35 -0700

    Edit: 0-puts_recursion.c

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -1,5 +1,5 @@
 #include"main.h"
-/*
+/**
  * _puts_recursion - print a string followed by a new line
  *
  * @s: the address of the string
@@ -8,8 +8,11 @@
  */
 void _puts_recursion(char *s)
 {
-	if(*s == '\0')
+	if (*s == '\0')
+	{
 		_putchar('\n');
+		return;
+	}
 	_putchar(*s);
 	_puts_recursion(s + 1);
 }
