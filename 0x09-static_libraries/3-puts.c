346254a80ca153c0df413cd13887120a3af41bef
Mahmoud EL-Sanour <mahmoudsanour@gmail.com>
2023-04-07 15:06:28 -0700

    Add: libmy.a

--- 0x09-static_libraries/3-puts.c
+++ 0x09-static_libraries/3-puts.c
@@ -1,3 +1,5 @@
+#include"main.h"
+
 /**
  * _puts - print a string
  *
@@ -8,4 +10,15 @@
 
 void _puts(char *str)
 {
+	int i;
+
+	for (i = 0; str[i] >= 0; i++)
+	{
+		if (str[i] == '\0')
+		{
+			_putchar('\n');
+			break;
+		}
+		_putchar(str[i]);
+	}
 }
