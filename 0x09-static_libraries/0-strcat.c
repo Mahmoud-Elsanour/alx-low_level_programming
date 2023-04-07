d4aadd6466d7c3187ffa068958d5dfb4af12b63f
Mahmoud Ibrahim El-Sanour <118695965+Mahmoud-Elsanour@users.noreply.github.com>
2023-04-07 23:38:14 +0200

    Create 0-strcat.c

---
+++ 0x09-static_libraries/0-strcat.c
@@ -0,0 +1,13 @@
+/**
+ * _strcat - function that concatenates
+ *          two strings.
+ *
+ * @dest: pointer to destnation input
+ * @src: pointer to source input
+ *
+ * Return: pointer to resulting string @dest
+*/
+
+char *_strcat(char *dest, char *src)
+{
+}
