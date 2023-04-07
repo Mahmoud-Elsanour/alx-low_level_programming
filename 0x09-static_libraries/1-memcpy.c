c7dc63c3cc569cac14119574d1ababd914e8ab07
Mahmoud Ibrahim El-Sanour <118695965+Mahmoud-Elsanour@users.noreply.github.com>
2023-04-07 23:40:43 +0200

    Create 1-memcpy.c

---
+++ 0x09-static_libraries/1-memcpy.c
@@ -0,0 +1,13 @@
+/**
+ * _memcpy - function that copies memory area
+ *
+ * @dest: buffer where we will copy to
+ * @src: what we are to copy
+ * @n: n bytes of @src
+ *
+ * Return: Always 0 (Success)
+*/
+
+char *_memcpy(char *dest, char *src, unsigned int n)
+{
+}
