85d580adb5234091b4e538a8abc44b72d5cdd740
Mahmoud Ibrahim El-Sanour <118695965+Mahmoud-Elsanour@users.noreply.github.com>
2023-04-07 23:47:25 +0200

    Create 4-isalpha.c

---
+++ 0x09-static_libraries/4-isalpha.c
@@ -0,0 +1,12 @@
+/**
+ * _isalpha - checks if character is alphabetic
+ *            both lowercase or uppercase
+ *
+ * @c: takes input from other functions.
+ *
+ * Return: 1 is c if true else 0
+*/
+
+int _isalpha(int c)
+{
+}
