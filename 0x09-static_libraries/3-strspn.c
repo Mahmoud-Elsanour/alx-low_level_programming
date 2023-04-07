4e817cd104a4d79a70cd2786b2614f97962268a9
Mahmoud Ibrahim El-Sanour <118695965+Mahmoud-Elsanour@users.noreply.github.com>
2023-04-07 23:46:54 +0200

    Create 3-strspn.c

---
+++ 0x09-static_libraries/3-strspn.c
@@ -0,0 +1,15 @@
+/**
+ * _strspn - a function that gets the
+ *           length of a prexif substring
+ *
+ * @s: pointer to string input
+ * @accept: substring prefix to look for
+ *
+ * Return: the number of bytes in the initial segment
+*/
+
+unsigned int _strspn(char *s, char *accept)
+{
+}
+
+	
