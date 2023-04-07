2e5ce40eb38c725d917bce074e1e20d72872e646
Mahmoud Ibrahim El-Sanour <118695965+Mahmoud-Elsanour@users.noreply.github.com>
2023-04-07 23:49:00 +0200

    Create 6-abs.c

---
+++ 0x09-static_libraries/6-abs.c
@@ -0,0 +1,12 @@
+/**
+ * _abs - function that computes the absolute
+ *       value of an integer
+ *
+ * @i: take input valaues for the function
+ *
+ * Return: Always 0 (Success)
+*/
+
+int _abs(int i)
+{
+}
