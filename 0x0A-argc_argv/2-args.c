2edf88a0ced5b8e008063c727ee79c26cae98d14
Mahmoud EL-Sanour <mahmoudsanour@gmail.com>
2023-04-08 13:17:36 -0700

    Edit: 2-args.c

--- 0x0A-argc_argv/2-args.c
+++ 0x0A-argc_argv/2-args.c
@@ -13,10 +13,6 @@ int main(int argc, char *argv[])
 	int i;
 
 	for (i = 0; i < argc; i++)
-	{
-		printf("%s", argv[i]);
-		if (i != argc - 1)
-			printf('\n');
-	}
+		printf("%s\n", argv[i]);
 	return (0);
 }
