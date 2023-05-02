7ceea3629038908353a4f7b1cce4b7fefb446502
Mahmoud El-Sanour <mahmoudsanour@gmail.com>
2023-05-02 03:49:36 -0700

    Edit: 0-print_listint.c

--- 0x13-more_singly_linked_lists/0-print_listint.c
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -10,13 +10,16 @@
 size_t print_listint(const listint_t *h)
 {
 	listint_t *head = h;
-	
+	size_t count = 0;
+
 	if (head == NULL)
 		return (0);
 	while (head != NULL)
 	{
 		printf("%d\n", head->n);
 		head = head->next;
+		count++;
 	}
 	printf("%d\n", head->n);
+	return (count);
 }
