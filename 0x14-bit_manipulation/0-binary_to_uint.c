99cd443c6ac38072c09dc1f29c3a503c51c44dc2
Mahmoud El-Sanour <mahmoudsanour@gmail.com>
2023-07-06 06:06:51 -0700

    Edit: 0

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -11,9 +11,11 @@ unsigned int binary_to_uint(const char *b)
 	int i;
 	unsigned int j = 0;
 
+	if (!b)
+		return (0);
 	for (i = 0; b[i] != '\0'; i++)
 	{
-		if ((b[i] != '0' && b[i] != '1') || *b == '\0')
+		if (b[i] != '0' && b[i] != '1')
 		{
 			return (0);
 		}
